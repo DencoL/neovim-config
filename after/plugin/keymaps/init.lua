6c48fedf69220b62b8e83e533f4c9e8813dc276f
SYGIC\dlochman <denis.lochman@protonmail.com>
2022-12-05 09:30:12 +0100

    added new keybindings

--- after/plugin/keymaps/init.lua
+++ after/plugin/keymaps/init.lua
@@ -15,3 +15,7 @@ nnoremap("grr", "<cmd>lua vim.lsp.codelens.refresh()<CR>")
 
 nnoremap("<leader>w", ":w<CR>")
 nnoremap("<leader>W", ":wa<CR>")
+
+nnoremap("<C-u>", "<C-u>zz")
+nnoremap("<C-d>", "<C-d>zz")
+nnoremap("n", "nzz")
