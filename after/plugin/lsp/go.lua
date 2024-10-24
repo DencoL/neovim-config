af5e71f1457c7684297f90ce8ff32fdd2aefb1a4
Denis Lochman <denis.lochman@protonmail.com>
2024-10-24 16:30:25 +0200

    feat: add go autoformat

--- after/plugin/lsp/go.lua
+++ after/plugin/lsp/go.lua
@@ -1,1 +1,7 @@
 require("denco.lsp_server").setup_server("gopls")
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+	pattern = '*.go',
+	group = vim.api.nvim_create_augroup('go_format', { clear = true }),
+    command = "lua vim.lsp.buf.format()"
+})
