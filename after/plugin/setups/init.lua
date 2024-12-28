3f23cc65b0e7fe83d808b1821aecf71fe8099973
DencoL <denis.lochman@protonmail.com>
2024-12-28 16:56:01 +0100

    refactor: migrate sign_define

--- after/plugin/setups/init.lua
+++ after/plugin/setups/init.lua
@@ -24,12 +24,6 @@ require("lualine").setup {
     }
 }
 
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
-
 require('illuminate').configure({
     providers = {
         'lsp',
