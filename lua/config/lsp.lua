8b5d48e68ecd4490baf155225e2296e79c730317
DencoL <denis.lochman@protonmail.com>
2025-04-03 12:20:18 +0200

    feat: add love2d lib to lua server

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -7,3 +7,17 @@ lsp.setup_server(
     { "dotnet", os.getenv("UserProfile") .. "/omnisharp/OmniSharp.dll" },
     { analyze_open_documents_only = true },
     false)
+
+require('lspconfig').lua_ls.setup {
+    settings = {
+        Lua = {
+            workspace = {
+                checkThirdParty = false,
+                telemetry = { enable = false },
+                library = {
+                    "$HOME/.local/bin/lua-lsp/meta/3rd/love2d"
+                }
+            }
+        }
+    }
+}
